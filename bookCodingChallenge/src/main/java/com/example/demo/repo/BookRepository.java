070c001fe829f8da0b3511a55beb1e83b69fe236
Harini1305 <165678461+Harini1305@users.noreply.github.com>
2026-05-02 13:03:16 +0530

    Delete bookCodingChallenge directory

--- bookCodingChallenge/src/main/java/com/example/demo/repo/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.entity.Book;
-
-public interface BookRepository extends JpaRepository<Book, String> {
-}
