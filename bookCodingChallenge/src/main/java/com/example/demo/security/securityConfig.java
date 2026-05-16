a2923e8a579fa1b17929500f13f3c65aabd5cdab
Harini1305 <165678461+Harini1305@users.noreply.github.com>
2026-05-16 12:38:03 +0530

    Add files via upload

--- bookCodingChallenge/src/main/java/com/example/demo/security/securityConfig.java
+++ bookCodingChallenge/src/main/java/com/example/demo/security/securityConfig.java
@@ -1,6 +1,8 @@
 package com.example.demo.security;
 
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -11,14 +13,18 @@ public class securityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.csrf().disable()
+        http
+            .cors()
+            .and()
+            .csrf().disable()
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/auth/**").permitAll()
-                .anyRequest().permitAll()
+                    .requestMatchers("/auth/**").permitAll()
+                    .anyRequest().permitAll()
             );
 
         return http.build();
     }
+
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
